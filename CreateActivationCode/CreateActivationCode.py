7d4ee089f599526e3bc6336860f45f13d033a469
zdyGit <1193009984@qq.com>
2016-04-05 21:54:16 +0800

    Add Project [CreateActivationCode]

---
+++ CreateActivationCode/CreateActivationCode.py
@@ -0,0 +1,15 @@
+import string
+import random
+
+
+def actication_code(key,length=20):
+	head = hex(key)[2:]
+	head = head+"HW"
+	chars = string.ascii_letters+string.digits
+	l = length-len(head)
+	tail = ''.join([random.choice(chars) for i in range(l)])
+	return head+tail
+
+
+L = [actication_code(i,8) for i in range(200)]
+print(L) 
